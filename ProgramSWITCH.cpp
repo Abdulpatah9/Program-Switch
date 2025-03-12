6bb2dce253e2d6d7b9ddbc290b76cea1f9cf8ed4
Abdul Patah <abdulpatah099@gmail.com>
2025-03-12 22:45:04 +0800

    Menambahkan Variabel dan Input Bilangan

---
+++ ProgramSWITCH.cpp
@@ -0,0 +1,18 @@
+// Program matematika sederhana menggunakan pernyataan switch-case
+// Abdul Patah NIM 230401010308
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Deklarasi Variabel - Untuk menyimpan dua bilangan, Pilihan operasi,  dan Hasil
+    int bil1, bil2, pilihan, hasil;
+
+    // Meminta pengguna untuk memasukan dua bilangan
+    cout <<"Masukan Bilangan 1: ";
+    cin >> bil1;
+    cout <<"Masukan Bilangan 2: ";
+    cin >> bil2;
+
+
+}
